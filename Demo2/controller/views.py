0d60c12fe82ddc89e426617c96f3f313a9ffad5b
ProdigyYanng <1376723538@qq.com>
2020-12-25 16:41:03 +0800

    可以实现汽车信息的展示的小demo

--- Demo2/controller/views.py
+++ Demo2/controller/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from login import models
 
 # Create your views here.
+
+def index(request):
+    all_cars = models.CarInfo.objects.all()
+    # for car in all_cars:
+    #     print(car)
+    #     print(car.pk)
+    #     print(car.Car_License)
+    #     print(car.Car_Type)
+    #     print(car.Car_Electric_Quantity)
+
+    return render(request, 'controller/controller.html', {'all_cars': all_cars})
